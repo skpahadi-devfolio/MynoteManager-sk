f5a6c6c8d2e6dfaa80b7137b29538b7fc73224ea
Sandeep <sandeepkohli180@gmail.com>
2026-03-22 15:38:34 +0530

    fix cors issue from cookies to JWT+head

--- backend/middleware/authmiddleware.js
+++ backend/middleware/authmiddleware.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
 export const authmiddleware = (req, res, next) => {
-    const token = req.cookies.token;
+    const token = req.headers.authorization.split(" ")[1];
 
     if(!token){
         return res.status(400).json({
@@ -11,7 +11,7 @@ export const authmiddleware = (req, res, next) => {
     }
 
     try {
-        const decoded = jwt.verify(token, "secretkey");
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded;
         next();
     } catch (error) {
--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,6 @@ import { connectDb } from "./config/db.js";
 import contactRoute from "./routes/contactRoute.js"
 import authRoute from "./routes/authRoute.js"
 import noteRoutes from "./routes/noteRoutes.js"
-import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 const app = express()
 const PORT = process.env.PORT || 3000
@@ -12,10 +11,10 @@ const PORT = process.env.PORT || 3000
 //dotenv:-
 dotenv.config();
 //accept different server to each other for comunication
-app.use(cors({origin: "https://mynotemanager-skweb.netlify.app",credentials: true}));
+app.use(cors());
 
 //http cookies for JWT:-
-app.use(cookieParser());
+// app.use(cookieParser());
 
 //middleware for everything
 app.use(express.json());
