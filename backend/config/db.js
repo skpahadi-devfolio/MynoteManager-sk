fac0e99fda53fa5b7e6a7e6fb13d2f06b75784ab
Sandeep <sandeepkohli180@gmail.com>
2026-03-22 12:08:45 +0530

    Convert localhost connection MONGO_URI

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDb = async() =>{
     try {
-        const conn = await mongoose.connect("mongodb://localhost:27017/MyNoteManagerapp")
+        const conn = await mongoose.connect(process.env.MONGO_URI)
         console.log("MongoDb is Connected SuccessFully!");
     } catch (error) {
         console.log("Database Connection failed:", error);
